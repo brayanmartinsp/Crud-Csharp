99203118cf35c6c04c14c3111f39c90a09beeba8
brayanmartinsp <brayanstilos@gmail.com>
2026-01-20 08:06:22 -0300

    foi

---
+++ MeuProjeto/MeuProjeto/Controles/ConexaoBanco.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace MeuProjeto.ConexaoBanco
+{
+	//class faz conexao com banco
+	public class ConexaoBanco
+	{
+		private static String connectionString =
+		   "Server=localhost;" +
+			"Port=3306;" +
+			"Database=cadastro_db;" +
+			"Uid=root;" +
+			"Pwd=;";
+
+		public static MySqlConnection Conectar()
+		{
+			MySqlConnection conexao = new MySqlConnection(connectionString);
+
+			try
+			{
+				conexao.Open();
+				//Console.WriteLine("Conectado com sucesso!");
+				return conexao;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("Erro ao conectar no MySQL: " + ex.Message);
+			}
+		}
+	}
+
+	// class cadastro
+	class CadastroDados
+	{
+		// funçao faz insert no  banco de dados
+		public void InserirDados(string nome, string cpf, int idade)
+		{
+			using (var con = ConexaoBanco.Conectar())
+			{
+				var sql = "INSERT INTO cadastropessoas (nome, cpf ,idade) VALUES (@nome ,@cpf ,@idade)";
+
+				using (var cmd = new MySqlCommand(sql, con))
+				{
+					cmd.Parameters.AddWithValue("@nome", nome);
+
+					cmd.Parameters.AddWithValue("@cpf", cpf);
+
+					cmd.Parameters.AddWithValue("@idade", idade);
+
+					cmd.ExecuteNonQuery();
+				}
+			}
+
+		}
+
+		// funçao para atualizar dados
+		public void AtualzarDados(int id, string nome, string cpf, int idade)
+		{
+			using (var con = ConexaoBanco.Conectar())
+			{
+				var sql = @"UPDATE cadastropessoas SET nome = @nome ,cpf = @cpf , idade = @idade  WHERE id = @id";
+
+				using (var cmd = new MySqlCommand(sql, con))
+				{
+					cmd.Parameters.AddWithValue("@id", id);
+
+					cmd.Parameters.AddWithValue("@nome", nome);
+
+					cmd.Parameters.AddWithValue("@cpf", cpf);
+
+					cmd.Parameters.AddWithValue("@idade", idade);
+
+					cmd.ExecuteNonQuery();
+				}
+			}
+
+		}
+
+		public bool DeleteUsuario(int id)
+		{
+			using (var con = ConexaoBanco.Conectar())
+			{
+				var sql = "DELETE FROM cadastropessoas WHERE id = @id";
+
+				using (var cmd = new MySqlCommand(sql, con))
+				{
+					cmd.Parameters.AddWithValue("@id", id);
+					int linhasAfet = cmd.ExecuteNonQuery();
+					return linhasAfet > 0;
+				}
+
+			}
+
+		}
+		public  void ListaDados()
+		{
+			using (var con = ConexaoBanco.Conectar())
+			{
+				string sql = "SELECT id, nome, cpf, idade FROM cadastropessoas";
+
+				using (var cmd = new MySqlCommand(sql, con))
+				using (var reader = cmd.ExecuteReader())
+				{
+
+					Console.WriteLine("\n--- Lista De Pessoas ---");
+
+					if (!reader.HasRows)
+					{
+						Console.WriteLine("Nenhum registro encontrado.");
+						return;
+					}
+
+					while (reader.Read())
+					{
+						Console.WriteLine($"ID: {reader.GetInt32("id")}");
+						Console.WriteLine($"Nome: {reader.GetString("nome")}");
+						Console.WriteLine($"CPF: {reader.GetString("cpf")}");
+						Console.WriteLine($"Idade: {reader.GetInt32("idade")}");
+						Console.WriteLine("----------------------");
+					}
+
+
+				}
+
+			}
+		}
+
+	}
+
+}
---
+++ MeuProjeto/MeuProjeto/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MeuProjeto.ConexaoBanco;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+	enum menuCadastro
+	{
+		Cadastrar = 1,
+		ListarDados = 2,
+		Atualizar = 3,
+		Deletar = 4,
+		Sair = 5
+	}
+	static void Main(string[] args)
+	{
+		MySqlConnection conexao = ConexaoBanco.Conectar(); 
+
+		int opcao;
+		Console.WriteLine("Conectado!");
+	
+		do
+		{
+            Console.WriteLine("");
+			Console.WriteLine("|===================================|");
+			Console.WriteLine("|         MENU DE CADASTRO          |");
+			Console.WriteLine("|===================================|");
+			Console.WriteLine("|         1   Cadastrar             |");
+			Console.WriteLine("|         2  Listar Dados           |");
+			Console.WriteLine("|         3   Atualizar             |");
+			Console.WriteLine("|         4    Deletar              |");
+			Console.WriteLine("|         5     Sair                |");
+			Console.WriteLine("|===================================|");
+            Console.WriteLine("");
+
+			opcao = int.Parse(Console.ReadLine());
+
+			switch ((menuCadastro)opcao)
+			{
+				case menuCadastro.Cadastrar:
+					CadastroDados Cadastrar = new CadastroDados();
+                   
+
+					Console.Write("Digite o nome: ");
+					string nome = Console.ReadLine();
+
+
+					Console.Write("Digite o CPF: ");
+					string cpf = Console.ReadLine();
+
+					Console.Write("Digite a idade: ");
+					int idade = int.Parse(Console.ReadLine());
+
+					Cadastrar.InserirDados(nome, cpf, idade);
+                    Console.WriteLine("");
+					Console.Write("Dados inseridos com sucesso!");
+                    Console.WriteLine("");
+					break;
+
+				case menuCadastro.Atualizar:
+					CadastroDados atualizar = new CadastroDados();
+					Console.Write("\nDigite o ID para atualizar: ");
+					Console.WriteLine();
+					int id = int.Parse(Console.ReadLine());
+
+					Console.Write("Digite o nome: ");
+					string atnome = Console.ReadLine();
+
+
+					Console.Write("Digite o CPF: ");
+					string atcpf = Console.ReadLine();
+
+					Console.Write("Digite a idade: ");
+					int atidade = int.Parse(Console.ReadLine());
+
+					atualizar.AtualzarDados(id, atnome, atcpf, atidade);
+                    Console.WriteLine("");
+					Console.Write("Atualizado com sucesso!");
+					Console.WriteLine("");
+					break;
+
+				case menuCadastro.ListarDados:
+					CadastroDados Listar = new CadastroDados();
+					Listar.ListaDados();
+					break;
+				case menuCadastro.Deletar:
+					CadastroDados deletar = new CadastroDados();
+
+					Console.Write("Digite o ID para deletar: ");
+					Console.WriteLine();
+					int idDelete = int.Parse(Console.ReadLine());
+					deletar.DeleteUsuario(idDelete);
+
+                    Console.WriteLine("");
+					Console.Write("Deletado com sucesso!");
+                    Console.WriteLine("");
+					break;
+				case menuCadastro.Sair:
+					Console.WriteLine("Saindo...");
+					break;
+
+				default:
+					Console.WriteLine("Opção inválida!");
+					break;
+			}
+
+		} while (opcao != 5);
+
+
+
+	}
+}
